f7c3eb5920d8ba48ce86fb613555197adfd29d1f
pinkypeach42 <elagina_daria@gmx.de>
2026-03-08 15:27:57 +0100

    func kantenliste

--- tests/kantenliste.cpp
+++ tests/kantenliste.cpp
@@ -3,13 +3,29 @@
 #include <iostream>
 #include <iostream>
 
+constexpr uint8_t ROWS = 4;
+constexpr uint8_t COLS = 8;
+constexpr uint8_t SCALE = 10;
 
+void FillCNConnections(int (&h) [ROWS][COLS], std::vector <uint8_t>& vec){
+    for (int row = 0; row < ROWS; row++){
+        
+        for (int col = 0; col < COLS; col++){
+            
+            if (h[row][col] >= 0) {
+                int8_t index = h[row][col]+col*SCALE;
+                //std::cout << "Index in Base Matrix: " << +(h[row][col]) << std::endl;
+                vec.push_back(index);
+                //std::cout << +(index) << std::endl;
+            }
+        }
+    }
+    
+}
 
 
 int main()
 {
-    constexpr uint8_t ROWS = 4;
-    constexpr uint8_t COLS = 8;
     
     // H Matrix
     int h [ROWS][COLS] = {
@@ -23,24 +39,12 @@ int main()
     
     std::vector <uint8_t> vec;
     vec.reserve(COLS*SCALE); // durch Dual Diagonal Form wird eig mehr als nötig allociert
-    //std::cout << +vec.capacity() << std::endl; //check wie viel allociert wurde
+
+    FillCNConnections(h,vec);
     
-    
-    for (int row = 0; row < ROWS; row++){
-        //std::cout << +h_row << std::endl;
-        
-        for (int col = 0; col < COLS; col++){
-            
-            if (h[row][col] >= 0) {
-                int8_t index = h[row][col]+col*SCALE;
-                std::cout << "Index in Base Matrix: " << +(h[row][col]) << std::endl;
-                vec.push_back(index);
-                std::cout << +(index) << std::endl;
-            }
-        }
-    }
-    
-    std::cout << +vec.size() << std::endl;
+    // ACHTUNG!!! Wegen Dual Diagonal Form hat 1. Check Node -1 weniger Verbindungen als der Rest
+    // in Decoder schleife berücksigtigen!
+
     
 
 
